5a5e4eaa806ca9083ed378dd1a907ce9b63560a3
Michał Hęćka <mhecka@205-dtir-michalh.local>
2022-06-01 15:51:34 +0200

    first commit

---
+++ notifications_test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  notifications_test
+//
+//  Created by Michał Hęćka on 01/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ notifications_test/notifications_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  notifications_testApp.swift
+//  notifications_test
+//
+//  Created by Michał Hęćka on 01/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct notifications_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
